392909d5361cfc544b0b2cc96d18998a93bed1cf
Chris Curry <Shadowx394@gmail.com>
2025-11-09 20:09:21 -0500

    More exercise 3 updates

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -180,7 +180,7 @@ class TestProductRoutes(TestCase):
         """It should not Get a Product thats not found"""
         response = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-        data = respons.get_json()
+        data = response.get_json()
         self.assertIn("was not found", data["message"])
 
     ######################################################################
